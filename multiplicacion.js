70c8765f313b281c1ef14efd11a5a06f126d9102
Alexis Viale <alexisviale@ALEXIS-5.local>
2021-11-25 21:36:01 -0300

    Se agregan operaciones matematicas

---
+++ multiplicacion.js
@@ -0,0 +1,5 @@
+const division = (num1, num2) => {
+    return num1*num2
+} 
+
+console.log(division(20, 10));
---
+++ suma.js
@@ -0,0 +1,5 @@
+const sums = (num1, num2) => {
+    return num1 + num2
+} 
+
+console.log(suma(95, 100));
